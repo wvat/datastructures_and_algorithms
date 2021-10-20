cf179245b6781f4c59d0915e0aaab976dad35573
Fred Vatnsdal <fred.vatnsdal@gmail.com>
2021-10-19 22:51:16 -0400

    Nest iterator class in linkedlist class

--- examples/doublylinkedlist/LinkedList.cpp
+++ examples/doublylinkedlist/LinkedList.cpp
@@ -1,13 +1,11 @@
 #include "LinkedList.h"
 
 template <class Type>
-Node<Type>::Node(const Type& val): item(val){
-    next = prev = 0;
-}
+Node<Type>* LinkedList<Type>::front = 0;
 
 template <class Type>
-LinkedList<Type>::LinkedList(){
-    front = back = 0;
+Node<Type>::Node(const Type& val): item(val){
+    next = prev = 0;
 }
 
 template <class Type>
@@ -49,109 +47,3 @@ template <class Type>
 bool LinkedList<Type>::is_empty(){
     return front == 0;
 }
-
-template <class Type>
-Iterator<Type>::Iterator(const LinkedList<Type> &ll){
-    list = ll;
-    current_node = list->front;
-}
-
-template <class Type>
-Iterator<Type>::~Iterator(){
-    delete list;
-}
-
-template <class Type>
-void Iterator<Type>::insert(const Type& val){
-    // if inserting at the end need to update back
-    Node<Type>* nn = new Node<Type>(val);
-
-    // update front and back as necessary
-    if(list->is_empty()){
-        list->front = list->back = nn;
-    }
-
-    else if(list->back == current_node){
-        list->back = nn;
-    }
-
-    nn->next = current_node->next;
-    nn->prev = current_node;
-    current_node->next->prev = nn;
-    current_node->next = nn;
-
-    // advance the iterator to now point at the newly inserted node
-    current_node = nn;
-}
-
-template <class Type>
-Type Iterator<Type>::remove(){
-    if(list->is_empty()){
-        cerr << "Error: can't remove node from an empty list.\n";
-        exit(-1);
-    }
-    else{
-        Node<Type>* ptr = current_node;
-        Type val = current_node->item;
-
-        if(current_node->prev){
-            current_node->prev->next = current_node->next;
-            current_node = ptr->prev;
-        }
-        else{
-            list->front = current_node->next; 
-            current_node = list->front;
-        }
-
-        if(current_node->next){
-            current_node->next->prev = current_node->prev;
-            current_node = ptr->next;
-        }
-        else{
-            list->back = current_node->prev;
-            current_node = list->back;
-        }
-        
-        delete ptr;
-        
-        return val;
-    }
-}
-
-// Prefix increment
-template<class Type>
-Iterator<Type>& Iterator<Type>::operator++()
-{
-    current_node = current_node->next;
-    return *this;
-}
-
-// Prefix decrement 
-template<class Type>
-Iterator<Type>& Iterator<Type>::operator--()
-{
-    current_node = current_node->prev;
-    return *this;
-}
-
-// Postfix increment
-template<class Type>
-Iterator<Type> Iterator<Type>::operator++(int i)
-{
-    // TODO use i for variable sized increments
-    Iterator<Type> temp = *this;
-    ++*this;
-    return temp;
-}
-
-// Postfix increment
-template<class Type>
-Iterator<Type> Iterator<Type>::operator--(int i)
-{
-    // TODO use i for variable sized increments
-    Iterator<Type> temp = *this;
-    --*this;
-    return temp;
-}
-
-
--- examples/doublylinkedlist/LinkedList.h
+++ examples/doublylinkedlist/LinkedList.h
@@ -6,9 +6,6 @@ template <class Type>
 class LinkedList;
 
 template <class Type>
-class Iterator;
-
-template <class Type>
 class Node{
     friend class LinkedList<Type>;
     friend ostream& operator<<(ostream &os, Node& n){
@@ -26,7 +23,6 @@ private:
 template <class Type>
 class LinkedList{
     friend class Node<Type>;
-    friend class Iterator<Type>;
     friend ostream& operator<<(ostream &os, LinkedList& ll){
         Node<Type>* ptr = 0;
         os << "Linked list contents:\n";
@@ -36,43 +32,113 @@ class LinkedList{
         return os;
     }
 public:
-    LinkedList();
+    LinkedList(){
+        front = back = 0;
+    }
     ~LinkedList();
     void add(const Type&);
     Type remove();
     bool is_empty();
+
+    class Iterator{
+    public:
+        Iterator() : current_node(front) {}
+        Iterator(const Node<Type>* node) : current_node(node) {}
+
+        // Prefix increment
+        Iterator& operator++()
+        {
+            current_node = current_node->next;
+            return *this;
+        }
+
+        // Prefix decrement 
+        Iterator& operator--()
+        {
+            current_node = current_node->prev;
+            return *this;
+        }
+
+        // Postfix increment
+        Iterator operator++(int i)
+        {
+            // TODO use i for variable sized increments
+            Iterator temp = *this;
+            ++*this;
+            return temp;
+        }
+
+        // Postfix increment
+        Iterator operator--(int i)
+        {
+            // TODO use i for variable sized increments
+            Iterator temp = *this;
+            --*this;
+            return temp;
+        }
+
+        /*void insert(const Type& val){
+            // if inserting at the end need to update back
+            Node<Type>* nn = new Node<Type>(val);
+
+            // update front and back as necessary
+            if(list->is_empty()){
+                list->front = list->back = nn;
+            }
+
+            else if(list->back == current_node){
+                list->back = nn;
+            }
+
+            nn->next = current_node->next;
+            nn->prev = current_node;
+            current_node->next->prev = nn;
+            current_node->next = nn;
+
+            // advance the iterator to now point at the newly inserted node
+            current_node = nn;
+        }
+
+        Type remove(){
+            if(list->is_empty()){
+                cerr << "Error: can't remove node from an empty list.\n";
+                exit(-1);
+            }
+            else{
+                Node<Type>* ptr = current_node;
+                Type val = current_node->item;
+
+                if(current_node->prev){
+                    current_node->prev->next = current_node->next;
+                    current_node = ptr->prev;
+                }
+                else{
+                    list->front = current_node->next; 
+                    current_node = list->front;
+                }
+
+                if(current_node->next){
+                    current_node->next->prev = current_node->prev;
+                    current_node = ptr->next;
+                }
+                else{
+                    list->back = current_node->prev;
+                    current_node = list->back;
+                }
+                
+                delete ptr;
+                
+                return val;
+            }
+        }*/
+
+    private:
+        Node<Type>* current_node;
+    };
+
 private:
-    Node<Type>* front;
+    static Node<Type>* front;
     Node<Type>* back;
-};
-
-template <class Type>
-class Iterator{
-    friend class LinkedList<Type>;
-    friend class Node<Type>;
-public:
-    Iterator(const LinkedList<Type>&);
-    ~Iterator();
-
-    // operator overloads
-    // ++ and --
-    // Prefix operator
-    Iterator<Type>& operator++();
-    Iterator<Type>& operator--();
-
-    // Postfix operator
-    Iterator<Type> operator++(int);
-    Iterator<Type> operator--(int);
-    
-    // dereference
-    // assignment
-    // comparison
-
-    void insert(const Type&);
-    Type remove();
-private:
-    LinkedList<Type>* list;
-    Node<Type>* current_node;
 };
 
 #endif
