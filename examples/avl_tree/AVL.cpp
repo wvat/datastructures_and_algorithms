f02d76767ffc8dd6f44d87f00f4fb813c6272abb
Fred Vatnsdal <fred.vatnsdal@gmail.com>
2021-10-25 23:31:40 -0400

    Add skeleton of avl implementation

--- examples/avl_tree/AVL.cpp
+++ examples/avl_tree/AVL.cpp
@@ -1,4 +1,19 @@
 #include "AVL.h"
 
 template<class Type>
+Node::Node(){}
+
+template<class Type>
 AVL::AVL(){}
+
+template<class Type>
+AVL::~AVL(){}
+
+template<class Type>
+bool AVL::is_empty(){}
+
+template<class Type>
+void AVL::insert(const Type& val){}
+
+template<class Type>
+Type AVL::remove(){}
--- examples/avl_tree/AVL.h
+++ examples/avl_tree/AVL.h
@@ -4,9 +4,13 @@
 template<class Type>
 class Node{
 public:
+    Node();
 private:
     Type item;
+    int height;
     Node* parent;
+    Node* left_tree;
+    Node* right_tree;
 };
 
 template<class Type>
@@ -14,7 +18,13 @@ class AVL{
 public:
     AVL();
     ~AVL();
-};
+    bool is_empty();
+    int get_height();
+    void insert(const Type&);
+    Type remove();
 
+private:
+    Node<Type>* root;
+};
 
 #endif
